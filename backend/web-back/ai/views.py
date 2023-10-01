7215babaa85a2d6ce0daf3ebcfb43d5c47719f4b
Ryosuke <ryosuke.kamada.1998@gmail.com>
2023-10-01 17:06:40 +0900

    utilsの追加

--- backend/web-back/ai/views.py
+++ backend/web-back/ai/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from django.views.generic import View, UpdateView, FormView
+from django.http import HttpResponse, JsonResponse
+from rest_framework.response import Response
+import urllib.request
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.utils.decorators import method_decorator
 
 # Create your views here.
+
+class Test(APIView):
+    # permission_classes = ()
+    # authentication_classes = ()
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse("ok")
