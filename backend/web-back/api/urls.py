c30a3b69a15800bce3ea03432cd5aa87fb702dc7
eREDROCK <rdrc136@gmail.com>
2023-10-02 20:39:24 +0900

    [Add]ディベート中の会話の処理をするDebateAPIを追加

--- backend/web-back/api/urls.py
+++ backend/web-back/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from .views import RandomThemeAPI
+from .views import DebateAPI
 
 
 urlpatterns = [
     path('randomtheme/', RandomThemeAPI.as_view()),
+    path('debate/', DebateAPI.as_view()),
 ]
