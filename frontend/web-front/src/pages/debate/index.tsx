0bf19f529a69ab5dbc4514e6a39ccd4bccf62e93
Ryosuke <ryosuke.kamada.1998@gmail.com>
2023-10-03 16:33:29 +0900

    [Add] next.jsno

---
+++ frontend/web-front/src/pages/debate/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledHello = styled.div`
+  color: red;
+  span {
+    color: blue;
+  }
+
+  @media (min-width: 768px) {
+    span {
+      display: block;
+    }
+  }
+`;
+
+const App = () => {
+  return (
+    <div>
+      {/* ↓「span」を追加 */}
+      <StyledHello>debate</StyledHello>
+      debate
+    </div>
+  );
+};
+
+export default App;
