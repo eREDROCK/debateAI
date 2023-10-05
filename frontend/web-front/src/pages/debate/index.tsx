c012f3e65d6b69da7f8456309e4a47514c78cdd8
Ryosuke <ryosuke.kamada.1998@gmail.com>
2023-10-05 21:58:57 +0900

    [Fix] CORSの設定

--- frontend/web-front/src/pages/debate/index.tsx
+++ frontend/web-front/src/pages/debate/index.tsx
@@ -16,18 +16,38 @@ const StyledHello = styled.div`
 `;
 
 const App = () => {
-  const initialValues = { message: "", title: "", flag: true };
+  const initialValues = {
+    message: [{ role: "user", content: "" }],
+    title: "たけのこ派",
+    flag: true,
+  };
 
   // APIにリクエストした回数をカウントする
   const [count, setCount] = useState(0);
   const [formValues, setFromValues] = useState(initialValues);
+  const [messageValues, setMessageFromValues] = useState(initialValues);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     await axios
       .post(
-        "http://localhost:8080/api/debate",
+        "http://localhost:8080/api/debate/",
         {
-          message: formValues.message,
+          message: [
+            {
+              role: "user",
+              content: "きのこの里より、たけのこの里のほうが美味しいです。",
+            },
+            {
+              role: "assistant",
+              content:
+                "私は「きのこの里のほうが美味しい」と主張します。きのこの里は、口どけの良いチョコレートと、ほんのりとしたきのこの風味が絶妙にマッチしています。また、その独特な食感も魅力の一つです。たけのこの里も美味しいですが、きのこの里の方がより多くの人々に愛されており、その証拠に売り上げも高いです。",
+            },
+            {
+              role: "user",
+              content: "きのこの里より、たけのこの里のほうが売上は高いですよ",
+            },
+          ],
           title: formValues.title,
           flag: formValues.flag,
         },
@@ -41,19 +61,26 @@ const App = () => {
       )
       .then((response) => {
         console.log("body:", response.status);
-        if (response.status === 200) {
-          setCount(count + 1);
-        }
+        // setCount(count + 1);
+        console.log(response.data);
       })
       .catch((error) => {
-        console.log(error.response.data);
+        // console.log("error.response.data");
       });
   };
 
   const handleChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
     // console.log(e.target);
     const { name, value } = e.target;
-    setFromValues({ ...formValues, [name]: value });
+    setFromValues({
+      ...formValues,
+      [name]: [
+        {
+          role: "user",
+          content: value,
+        },
+      ],
+    });
     console.log(formValues);
   };
 
@@ -78,7 +105,10 @@ const App = () => {
           name="message"
           onChange={(e) => handleChanged(e)}
         />
+        <button>送信</button>
       </form>
+
+      {formValues.message[0].content}
     </div>
   );
 };
