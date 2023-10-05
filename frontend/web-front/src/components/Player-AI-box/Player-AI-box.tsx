76c407e59732548e34aeb9937412b610886d3396
eREDROCK <rdrc136@gmail.com>
2023-10-05 16:02:42 +0900

    [Add]フォームに入力中かどうかの処理完成

--- frontend/web-front/src/components/Player-AI-box/Player-AI-box.tsx
+++ frontend/web-front/src/components/Player-AI-box/Player-AI-box.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from "styled-components";
 
-const  Player=(role: number)=>{
+const  Player=({role, isInputting}: {role: number; isInputting: boolean })=>{
 
-  if (role==0) {
+  if (role === 0) {
     return(
       <div>
-        プレイヤーです
+         {isInputting ? (<div>応答中です</div>) : (<div>考え中です</div>)}
       </div>
     );
   } else {
@@ -20,3 +20,30 @@ const  Player=(role: number)=>{
 
 
 export default Player;
+
+
+// ゲーム画面に合体するときは以下の部分を入れてください
+// const [isInputting, setIsInputting] = useState(false);
+//   // formの入力中に呼び出す関数
+//   const handleInput = () => {
+//     setIsInputting(true);
+//   };
+//   // formに入力も何もしていないときに呼び出す関数
+//   const handleInputBlur = () => {
+//     setIsInputting(false);
+//   };
+//
+
+//   formのinputにonFocusとonBlurを
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         placeholder="テキストを入力してください"
+//         onFocus={handleInput}// formに入力中の時にhandleInputを呼び出す
+//         onBlur={handleInputBlur}// formに入力も何もしていないときにhandleInputBlurを呼び出す
+//       />
+//     <Player role={0} isInputting={isInputting}/> //ユーザー画像を呼び出したいときはrole={0} AI画像を呼び出したいときはrole={1}
+//     <button>botann</button>
+//     </div>
+//   );
--- frontend/web-front/src/pages/test/redrcok.tsx
+++ frontend/web-front/src/pages/test/redrcok.tsx
@@ -1,10 +1,29 @@
 import Player from "../../components/Player-AI-box/Player-AI-box";
-import Title from "../../components/Title-box/Title-box"
+import React, { useState, useEffect } from "react";
+
+
 
 const App = () => {
+
+  const [isInputting, setIsInputting] = useState(false);
+  // formの入力中に呼び出す関数
+  const handleInput = () => {
+    setIsInputting(true);
+  };
+  // formに入力も何もしていないときに呼び出す関数
+  const handleInputBlur = () => {
+    setIsInputting(false);
+  };
   return (
     <div>
-    <Player role={0} />
+      <input
+        type="text"
+        placeholder="テキストを入力してください"
+        onFocus={handleInput}// formに入力中の時にhandleInputを呼び出す
+        onBlur={handleInputBlur}// formに入力も何もしていないときにhandleInputBlurを呼び出す
+      />
+    <Player role={0} isInputting={isInputting}/> 
+    <button>botann</button>
     </div>
   );
 };
