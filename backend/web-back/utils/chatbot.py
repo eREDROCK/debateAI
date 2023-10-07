de3cfba0a3d74c453ccdbd11128ad9d6ecff4bc9
Ryosuke <ryosuke.kamada.1998@gmail.com>
2023-10-08 02:30:09 +0900

    [Update] チャット昨日の実装

--- backend/web-back/utils/chatbot.py
+++ backend/web-back/utils/chatbot.py
@@ -15,25 +15,41 @@ openai.api_key      = os.getenv("API_KEY")
 def Ask_ChatGPT(message,title,role):
 
     messages = [{
-        "role":"system", "content":"あなたはディベートのスペシャリストです。これから私とあなたでディベートを行います。あなたはテーマに対して肯定派、否定派のいずれかとして私と討議します。その中であなたは私への説得、発言のタイミングや戦略、私の意見への反論などを行い、あなたの意見がより納得できる内容であることを読み手に伝わるように発言してください。 初めに「テーマ」とあなたと私、いずれが「肯定派」「否定派」かの指定を行い、その後、私とあなたでこのテーマについて繰り返し議論を行います。それでは始めます。テーマは「" + title + "」、あなたは「" + role + "」です。必ず60文字以内に返答してください。"
+        "role":"system", "content":"あなたはディベートのスペシャリストです。これから私とあなたでディベートを行います。あなたはテーマに対して肯定派、否定派のいずれかとして私と討議します。その中であなたは私への説得、発言のタイミングや戦略、私の意見への反論などを行い、あなたの意見がより納得できる内容であることを読み手に伝わるように発言してください。 初めに「テーマ」とあなたと私、いずれが「肯定派」「否定派」かの指定を行い、その後、私とあなたでこのテーマについて繰り返し議論を行います。それでは始めます。テーマは「" + title + "」、あなたは「" + role + "」です。端的に50文字程度で返答してください。"
     }]
 
     messages = messages + message 
     print(messages)
     
     # 応答設定
-    response = completion = openai.ChatCompletion.create(
-                 model    = "gpt-3.5-turbo",     # モデルを選択
-                 messages = messages,
-                 max_tokens  = 1024,             # 生成する文章の最大単語数
-                 n           = 1,                # いくつの返答を生成するか
-                 stop        = None,             # 指定した単語が出現した場合、文章生成を打ち切る
-                 temperature = 0.5,              # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
-    )
+    try:
+        response = openai.ChatCompletion.create(
+                    model    = "gpt-3.5-turbo",     # モデルを選択
+                    messages = messages,
+                    max_tokens  = 2048,             # 生成する文章の最大単語数
+                    n           = 1,                # いくつの返答を生成するか
+                    stop        = None,             # 指定した単語が出現した場合、文章生成を打ち切る
+                    temperature = 0.5,              # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
+        )
+    
+    except openai.error.APIError as e:
+        #Handle API error here, e.g. retry or log
+        print(f"OpenAI API returned an API Error: {e}")
+        pass
+    except openai.error.APIConnectionError as e:
+        #Handle connection error here
+        print(f"Failed to connect to OpenAI API: {e}")
+        pass
+    except openai.error.RateLimitError as e:
+        #Handle rate limit error (we recommend using exponential backoff)
+        print(f"OpenAI API request exceeded rate limit: {e}")
+        pass
     
     # 応答
+    print(response)
     # response = completion.choices[0].message.content
     response = response["choices"][0]["message"]["content"]
+    print("ok")
     
     # 応答内容出力
     return response
@@ -51,16 +67,32 @@ def Judge_debate(message):
 
     messages = messages + [{"role":"user", "content":content}]
 
-    response = completion = openai.ChatCompletion.create(
-                 model    = "gpt-3.5-turbo",     # モデルを選択
-                 messages = messages,
-                 max_tokens  = 1024,             # 生成する文章の最大単語数
-                 n           = 1,                # いくつの返答を生成するか
-                 stop        = None,             # 指定した単語が出現した場合、文章生成を打ち切る
-                 temperature = 0.5,              # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
-                )
+    # 応答設定
+    try:
+        response = openai.ChatCompletion.create(
+                    model    = "gpt-3.5-turbo",     # モデルを選択
+                    messages = messages,
+                    max_tokens  = 2048,             # 生成する文章の最大単語数
+                    n           = 1,                # いくつの返答を生成するか
+                    stop        = None,             # 指定した単語が出現した場合、文章生成を打ち切る
+                    temperature = 0.5,              # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
+        )
+    
+    except openai.error.APIError as e:
+        #Handle API error here, e.g. retry or log
+        print(f"OpenAI API returned an API Error: {e}")
+        pass
+    except openai.error.APIConnectionError as e:
+        #Handle connection error here
+        print(f"Failed to connect to OpenAI API: {e}")
+        pass
+    except openai.error.RateLimitError as e:
+        #Handle rate limit error (we recommend using exponential backoff)
+        print(f"OpenAI API request exceeded rate limit: {e}")
+        pass
     
     response = response["choices"][0]["message"]["content"]
+    print(response)
     # response = message + [{"role":"assistant", "content":response}]
     
     # 応答内容出力
@@ -76,7 +108,7 @@ def DefaetedJudge(message):
     response = completion = openai.ChatCompletion.create(
                  model    = "gpt-3.5-turbo",     # モデルを選択
                  messages = messages,
-                 max_tokens  = 1024,             # 生成する文章の最大単語数
+                 max_tokens  = 2048,             # 生成する文章の最大単語数
                  n           = 1,                # いくつの返答を生成するか
                  stop        = None,             # 指定した単語が出現した場合、文章生成を打ち切る
                  temperature = 0.5,              # 出力する単語のランダム性（0から2の範囲） 0であれば毎回返答内容固定
